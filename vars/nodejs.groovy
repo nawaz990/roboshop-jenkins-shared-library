2b3ed4aeb35d10a71a69975e6f4e749e14a7249b
nawaz990 <yasirnawaz990@gmail.com>
2022-12-29 01:25:47 +0530

    checking

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -1,21 +1,25 @@
 def call() {
-  nodejs {
+  pipeline {
+    agent any
+    stages {
 
-    stage("Code Quality") {
-      steps {
-        echo "Code Quality"
+
+      stage('Code Quality') {
+        steps {
+          echo 'Code Quality'
+        }
       }
-    }
 
-    stage("Test Cases") {
-      steps {
-        echo "Test Cases"
+      stage('Test Cases') {
+        steps {
+          echo 'Test Cases'
+        }
       }
-    }
 
-    stage("Publish A Release") {
-      steps {
-        echo "Publish A Release"
+      stage('Publish A Release') {
+        steps {
+          echo 'Publish A Release'
+        }
       }
     }
   }
