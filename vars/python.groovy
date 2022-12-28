8bbecf5e10204e6e999d4648a40b945871b16254
nawaz990 <yasirnawaz990@gmail.com>
2022-12-28 13:52:15 +0530

    changing  groovy connecting nodejs python & java

---
+++ vars/python.groovy
@@ -0,0 +1,26 @@
+def call() {
+  pipeline {
+  agent any
+
+    stages {
+
+      stage('Code Quality') {
+        steps {
+          echo "Code Quality"
+        }
+      }
+
+      stage('Test Cases') {
+        steps {
+          echo 'Test Cases'
+        }
+      }
+
+      stage('Publish A Release') {
+        steps {
+          echo 'Publish A Release'
+        }
+      }
+    }
+  }
+}
