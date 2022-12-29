1ad1d7d7891dc737875fe5cc3affa6e1de1144ef
nawaz990 <yasirnawaz990@gmail.com>
2022-12-29 18:23:02 +0530

    giving env.repourl for shipping and connecting it with  java groovy file

--- vars/java.groovy
+++ vars/java.groovy
@@ -1,24 +1,24 @@
 def call() {
   node {
 
+    stage('Checkout Code') {
+      git branch: 'main', url: "${env.REPO_URL}"
+    }
+
       stage('Compile') {
         echo 'Compile'
       }
 
-
       stage('Code Quality') {
           echo 'Code Quality'
-
       }
 
       stage('Test Cases') {
           echo 'Test Cases'
-
       }
 
       stage('Publish A Release') {
           echo 'Publish A Release'
-
      }
   }
 }
